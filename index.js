54b81922b65b44e6d476d755a5c743c72ced0769
choi3179 <jacobis@naer.com>
2024-01-25 14:17:08 +0900

    javascript CLI package 입력 및 랜덤 출력 기능 추가하기
    - [x] PR 생성
    - [ ] 요구사항 작성
    - [ ] 설계 작성

--- index.js
+++ index.js
@@ -20,13 +20,8 @@ console.log(`
 
 const argv = process.argv;
 
-if (argv.length < 4) {
-  console.log("사용법: index.js 홍길동 임꺽정 이순신");
-  process.exit(1);
-}
-
 const names = argv.slice(2).slice(0, -1);
 
 const randomName = names[Math.floor(Math.random() * names.length)];
 
-console.log(`발표자:${randomName}`);
+console.log(`발표자: ${randomName}`);
