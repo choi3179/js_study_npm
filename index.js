e2092ef3b140cb426cfe4ad2ffdff11880135ad8
choi3179 <jacobis@naer.com>
2024-01-25 17:23:36 +0900

    [MOD] 01.25 입력 관련 예외처리

--- index.js
+++ index.js
@@ -35,6 +35,10 @@ class Presenter {
 const argv = process.argv;
 const names = argv.slice(2);
 
+if( names.length < 2 ) {
+	console.log('두 명 이상 입력해 주시기 바랍니다.');
+	process.exit();
+}
 var presenter = new Presenter(names);
 presenter.select();
 
