6e9b17571c3dece93e97c2e8190883e4f4a0d03d
choi3179 <jacobis@naer.com>
2024-01-25 13:33:56 +0900

    js study
    - ex09.js : Pizza Chicken & Store Quiz

---
+++ js_0125/ex02.js
@@ -0,0 +1,21 @@
+// Inner Function
+
+const value = 'hello!';
+
+function myFunction() {
+	const value = 'bye!';
+	const anotherValue = 'world';
+
+	function functionInside() {
+		console.log('functionInside : ');
+		console.log(value);
+		console.log(anotherValue);
+	}
+
+	functionInside();
+}
+
+myFunction();
+console.log('global scope : ');
+console.log(value);
+//console.log(anotherValue);
---
+++ js_0125/ex09.js
@@ -0,0 +1,39 @@
+// 상속 관련 예제
+
+class Food {
+	constructor(name){
+		this.name = name;
+		this.brands = [];
+	}
+
+	addBrand(brand){
+		this.brands.push(brand);
+	}
+
+	print(){
+		console.log(`${this.name}을/를 파는 음식점들 : `);
+		console.log(this.brands.join(', '));
+	}
+}
+
+class Pizza extends Food {
+	constructor(name){
+		super(name);
+	}
+}
+
+class Chicken extends Food {
+	constructor(name){
+		super(name);
+	}
+}
+
+const pizza = new Pizza('피자');
+pizza.addBrand('피자헛');
+pizza.addBrand('도미노 피자');
+pizza.print();
+
+const chicken  = new Chicken('치킨');
+chicken.addBrand('굽네치킨');
+chicken.addBrand('BBQ');
+chicken.print();
