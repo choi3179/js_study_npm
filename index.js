19bc9e00af7ab08ad6fe29039992398c791514e6
choi3179 <jacobis@naer.com>
2024-01-29 11:37:53 +0900

    [MOD] 01.29 리팩토링 완료

--- index.js
+++ index.js
@@ -20,18 +20,6 @@ console.log(`
                                                                                 
 `);
 
-
-class Presenter {
-	constructor(presenters){
-		this.presenters = presenters;
-	}
-
-	select(){
-		const randomName = names[Math.floor(Math.random() * names.length)];
- 		console.log(`선택된 발표자 : ${randomName}`); 
-	}
-}
-
 const argv = process.argv;
 const names = argv.slice(2);
 
@@ -39,6 +27,10 @@ if( names.length < 2 ) {
 	console.log('두 명 이상 입력해 주시기 바랍니다.');
 	process.exit();
 }
-var presenter = new Presenter(names);
-presenter.select();
 
+function select(...names) {
+	const randomName = names[Math.floor(Math.random() * names.length)];
+	console.log(`선택된 발표자 : ${randomName}`);
+}
+
+select(...names);
